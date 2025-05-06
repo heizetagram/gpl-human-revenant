2de8fdc14baf8ff39833e0eb131136b244886331
Gabriel Wunder <gabriel.wunder2005@gmail.com>
2025-05-06 17:13:18 +0900

    initial commit

---
+++ Neon_Revenant/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+    }
+}
