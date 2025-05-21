ba004cdceb6763bbaba53b2b8209fe23fa6c0bbe
David Lu <davidlu1999@gmail.com>
2025-05-22 04:57:37 +0900

    - Added enemy - Added enemy animations - Able to kill enemy

--- Neon_Revenant/Assets/Scripts/Bullet.cs
+++ Neon_Revenant/Assets/Scripts/Bullet.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     public float speed = 20f;
+    public int damage = 40;
     public Rigidbody2D rb;
 
     private int direction = 1;
@@ -18,6 +19,11 @@ public class Bullet : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
         Destroy(gameObject);
     }
 }
---
+++ Neon_Revenant/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;
+    public Animator animator;
+
+    public void Update()
+    {
+        float speed = Mathf.Abs(GetComponent<Rigidbody2D>().linearVelocity.x);
+
+        animator.SetBool("isRunning", speed != 0);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        animator.SetTrigger("Hit");
+
+        if (health <= 0)
+        {
+            animator.SetTrigger("Death");
+            StartCoroutine(WaitAndDie(0.5f));
+        }
+    }
+
+    void Die()
+    {
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator WaitAndDie(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Die();
+    }
+}
