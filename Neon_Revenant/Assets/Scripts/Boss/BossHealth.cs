809a7cfa6615ea9c63d8f5f5edfe8cf60cacf40f
Gabriel Wunder <gabriel.wunder2005@gmail.com>
2025-06-01 11:32:56 +0900

    ADD healthbar for boiss

--- Neon_Revenant/Assets/Scripts/Boss/BossHealth.cs
+++ Neon_Revenant/Assets/Scripts/Boss/BossHealth.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BossHealth : MonoBehaviour
 {
-
+    public HealthBar healthbar;
     public int health = 500;
 
     public GameObject deathEffect;
 
     public bool isInvulnerable = false;
-
+    
     public void TakeDamage(int damage)
     {
         if (isInvulnerable)
             return;
 
         health -= damage;
-
+        healthbar.UpdateHealthBar(200, health);
         if (health <= 200)
         {
             GetComponent<Animator>().SetBool("IsEnraged", true);
@@ -31,7 +32,7 @@ public class BossHealth : MonoBehaviour
 
     void Die()
     {
-        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        GetComponent<Animator>().SetTrigger("Death");
         Destroy(gameObject);
     }
 
