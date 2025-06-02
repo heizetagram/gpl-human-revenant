87466abc22f3a69345d85f33c7ff79765fca075b
Gabriel Wunder <gabriel.wunder2005@gmail.com>
2025-06-02 12:02:06 +0900

    ADD barcode UI

--- Neon_Revenant/Assets/Scripts/BarcodeLoot.cs
+++ Neon_Revenant/Assets/Scripts/BarcodeLoot.cs
@@ -5,12 +5,16 @@ public class BarcodeLoot : MonoBehaviour
     public int value = 1;
 
     void OnTriggerEnter2D(Collider2D other)
-    {   
+    {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Barcode eingesammelt! +" + value);
+
+            // Barcode dem Manager hinzufügen
+            if (BarcodeManager.Instance != null)
+                BarcodeManager.Instance.AddBarcode(value);
+
             Destroy(gameObject);
         }
     }
 }
-
